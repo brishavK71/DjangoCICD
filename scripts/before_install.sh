563a3bf639b54badf8ba39232ff49de60404c33a
KuikHeLL <brishav.kuikel7@gmail.com>
2023-07-17 16:06:29 +0545

    edited for linux

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
 
-# clean codedeploy-agent files for a fresh install
-sudo rm -rf /home/ubuntu/install
-
-# install CodeDeploy agent
-sudo apt-get -y update
-sudo apt-get -y install ruby
-sudo apt-get -y install wget
-cd /home/ubuntu
-wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
-sudo chmod +x ./install 
-sudo ./install auto
-
 # update os & install python3
 sudo apt-get update
 sudo apt-get install -y python3 python3-dev python3-pip python3-venv
 pip install --user --upgrade virtualenv
 
 # delete app
-sudo rm -rf /home/ubuntu/django-aws_cicd
+sudo rm -rf /home/ec2-user/django-aws_cicd
