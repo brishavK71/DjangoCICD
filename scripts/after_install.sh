b84f3a2e7ef1b098ad9612badfd28d6259d0cb7e
KuikHeLL <brishav.kuikel7@gmail.com>
2023-07-19 16:32:25 +0545

    edited

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -14,8 +14,9 @@ python3 -m venv venv
 source venv/bin/activate
 
 # install requirements.txt
-pip install -r /home/ec2-user/django-aws_cicd/requirements.txt
 pip install django
+pip install -r /home/ec2-user/django-aws_cicd/requirements.txt
+
 
 # run server
 screen -d -m python3 manage.py runserver 0:8000
