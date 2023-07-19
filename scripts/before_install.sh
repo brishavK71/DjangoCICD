50c8eb103afaea862c19f8371ce5f820d75d67c1
KuikHeLL <brishav.kuikel7@gmail.com>
2023-07-19 16:02:45 +0545

    hello

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,11 +6,8 @@ sudo yum groupinstall "Development Tools"
 sudo yum install python3-devel
 sudo yum install gcc
 
-
 # update os & install python3
-sudo yum update
-sudo yum install -y python3 python3-dev python3-pip python3-venv
+sudo yum install -y pip
+sudo yum install -y python3
 pip install --user --upgrade virtualenv
 
-# delete app
-sudo rm -rf /home/ec2-user/django-aws_cicd
